c6c3301f0cd692e99e180f4876c71af4099c078b
qropa <124854119+qropa@users.noreply.github.com>
2024-02-20 16:18:45 +0900

    Update js

--- AHCOutputFileReaderForAllBrowsers.js
+++ AHCOutputFileReaderForAllBrowsers.js
@@ -2,27 +2,24 @@
 // @name         AHC Output File Reader for All Browsers
 // @namespace    https://github.com/qropa/AHCOutputFileReaderForAllBrowsers
 // @version      0.1
-// @description  AHCのビジュアライザに出力結果を自動で貼り付けます。breveなどのブラウザでも動作します。
+// @description  AHCのビジュアライザに出力結果を自動で貼り付けます。braveなどのブラウザでも動作します。
 // @author       ssaattoo
 // @match        https://img.atcoder.jp/*
 // @grant        none
 // ==/UserScript==
 
 if (!document.getElementById("dirSelect")) {
-  // 新たな<input>要素を作成
   const dirSelectElement = document.createElement("input");
   dirSelectElement.type = "file";
   dirSelectElement.id = "dirSelect";
   dirSelectElement.setAttribute("directory", "");
   dirSelectElement.setAttribute("webkitdirectory", "");
 
-  // <body>要素の子として新たな<input>要素を追加
   const output = document.getElementById("output");
   output.parentNode.insertBefore(dirSelectElement, output.previousElementSibling);
 }
 
 if (!document.getElementById("fileSelect")) {
-  // bodyの一番下に空の要素を作成
   const fileSelectElement = document.createElement("div");
   fileSelectElement.id = "fileSelect";
   document.body.appendChild(fileSelectElement);
@@ -81,12 +78,10 @@ errorBox.setAttribute("data-gramm_editor", "false");
 const output = document.getElementById("output");
 output.parentNode.insertBefore(errorBox, output.nextElementSibling);
 
-// 全ての<details>要素を取得
+// details要素を閉じる
 var detailsElements = document.getElementsByTagName("details");
 
-// 各<details>要素に対してループを回す
 for (var i = 0; i < detailsElements.length; i++) {
-  // <details>要素が開いている場合は閉じる
   if (detailsElements[i].open) {
     detailsElements[i].open = false;
   }
